fa0faf3c9a16f00bf18cb9a338a6ee28f7014536
Harsh29301 <harshadmdeshmukh29@gmail.com>
2023-08-02 14:42:55 +0530

    First commit

---
+++ FoodPlaza.session.sql
@@ -0,0 +1,20 @@
+use project1;
+
+/* CREATE TABLE Customer(
+    ctr_Name VARCHAR(200) NOT NULL,
+    emailId VARCHAR(200) NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    phoneno VARCHAR(30),
+    address text
+);
+
+*/
+
+
+/*ALTER TABLE Customer add constraint pk_customer primary key (emailId);
+
+*/
+
+desc Customer;
+
+select * FROM Customer;
