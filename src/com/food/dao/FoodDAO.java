0e353bc28fab4c3969f03c6c0146036dfc7eb68a
Harsh29301 <harshadmdeshmukh29@gmail.com>
2023-08-03 20:16:29 +0530

    FoodDAO interface is created with some abstract methods which get implemented in FoodDAOImpl in DAO implementation pkg

---
+++ src/com/food/dao/FoodDAO.java
@@ -0,0 +1,24 @@
+package com.food.dao;
+
+import java.util.List;
+
+import com.food.pojo.Food;
+
+public interface FoodDAO {
+
+    boolean addFood(Food food);
+
+    boolean updateFood(Food food);
+
+    boolean deleteFood(int foodId);
+
+    Food viewFoodbyFoodId(int foodId);
+
+    List<Food> viewAllFood();
+
+    List<Food> viewFoodsbyFoodName(String foodName);
+
+    List<Food> viewFoodsbyFoodType(String foodType);
+
+    List<Food> viewFoodbyFoodCategory(String category);
+}
